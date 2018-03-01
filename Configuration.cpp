e53ff983500c49171666b5f9f5bff6b80b806883
Stratmann <stratmann@ibt014.ibt.kfa-juelich.de>
2018-03-01 16:21:40 +0100

    Added more configurable values

--- Configuration.cpp
+++ Configuration.cpp
@@ -10,6 +10,13 @@
 const double Configuration::RADIAL_TEXT_FACTOR = 0.3;
 
 const double Configuration::COORDGRID_ADJUSTMENT = 0.5;
+const double Configuration::COORDPOINT_ANGLE = 0.005;
+
+const double Configuration::END_RADIUS_MAJOR_FACTOR = 0.25;
+const double Configuration::END_RADIUS_MINOR_FACTOR = 0.125;
+const double Configuration::RADIUS_TICK_LABEL_FACTOR = 0.75;
+const double Configuration::RADIUS_LABEL_FACTOR = 3;
+const double Configuration::RADIUS_HISTOGRAM_FACTOR = 10;
 
 const double Configuration::DATA_LINK_LINE_WIDTH = 0.1;
 
@@ -30,10 +37,7 @@ Configuration::Configuration()
 {}
 
 Configuration::Configuration(const std::string & fname, std::size_t inputs, const std::string & cpath)
-: _input_file(fname), _num_inputs(inputs),
-  mooviec_int("moovie\\.\\w+=\\d+"),
-  mooviec_dbl("moovie\\.\\w+=(\\d+(\\.\\d+)?|\\.\\d+)"),
-  mooviec_name("moovie\\.\\w+=[a-zA-Z ]+")
+: _input_file(fname), _num_inputs(inputs)
 {
 	std::string content = Util::read_file(cpath);
 
@@ -44,115 +48,116 @@ Configuration::Configuration(const std::string & fname, std::size_t inputs, cons
 		{
 			std::string stripped_line = Util::strip(line);
 
-			if (std::regex_match(stripped_line, mooviec_int))
-			{
-				const std::vector<std::string> & definition = Util::split(stripped_line, "=");
+			std::size_t split_pos = stripped_line.find_first_of("=");
+			const std::string & key = Util::strip(stripped_line.substr(0, split_pos));
+			const std::string & value = Util::strip(stripped_line.substr(split_pos + 1,
+					stripped_line.size() - split_pos - 1));
 
-				if (definition[0].compare("moovie.width") == 0)
-				{
-					_width = Util::string_to_int(definition[1]);
-				}
-				else if (definition[0].compare("moovie.height") == 0)
-				{
-					_height = Util::string_to_int(definition[1]);
-				}
-				else if (definition[0].compare("moovie.input_inner_radius") == 0)
-				{
-					_input_inner_radius = Util::string_to_double(definition[1]);
-				}
-				else if (definition[0].compare("moovie.input_thickness") == 0)
-				{
-					_input_thickness = Util::string_to_double(definition[1]);
-				}
-				else if (definition[0].compare("moovie.output_angle_span") == 0)
-				{
-					_output_angle_span = Util::string_to_double(definition[1]);
-				}
-				else if (definition[0].compare("moovie.output_inner_radius") == 0)
-				{
-					_output_inner_radius = Util::string_to_double(definition[1]);
-				}
-				else if (definition[0].compare("moovie.output_thickness") == 0)
-				{
-					_output_thickness = Util::string_to_double(definition[1]);
-				}
-				else if (definition[0].compare("moovie.grid_size") == 0)
-				{
-					_grid_size = Util::string_to_double(definition[1]);
-				}
-				else if (definition[0].compare("moovie.tick_label_font_size") == 0)
-				{
-					_tick_label = TextProperties(_tick_label.fontname(), Util::string_to_double(definition[1]));
-				}
-				else if (definition[0].compare("moovie.var_label_font_size") == 0)
-				{
-					_var_label = TextProperties(_var_label.fontname(), Util::string_to_double(definition[1]));
-				}
-				else if (definition[0].compare("moovie.thick_line") == 0)
-				{
-					_prop_thick.line_width = Util::string_to_double(definition[1]);
-				}
-				else if (definition[0].compare("moovie.thick_line") == 0)
-				{
-					_prop_thin.line_width = Util::string_to_double(definition[1]);
-				}
-			}
-			else if (std::regex_match(stripped_line, mooviec_dbl))
+			if (key.compare("moovie.width") == 0)
 			{
-				const std::vector<std::string> & definition = Util::split(stripped_line, "=");
-
-				if (definition[0].compare("moovie.input_inner_radius") == 0)
-				{
-					_input_inner_radius = Util::string_to_double(definition[1]);
-				}
-				else if (definition[0].compare("moovie.input_thickness") == 0)
-				{
-					_input_thickness = Util::string_to_double(definition[1]);
-				}
-				else if (definition[0].compare("moovie.output_angle_span") == 0)
-				{
-					_output_angle_span = Util::string_to_double(definition[1]);
-				}
-				else if (definition[0].compare("moovie.output_inner_radius") == 0)
-				{
-					_output_inner_radius = Util::string_to_double(definition[1]);
-				}
-				else if (definition[0].compare("moovie.output_thickness") == 0)
-				{
-					_output_thickness = Util::string_to_double(definition[1]);
-				}
-				else if (definition[0].compare("moovie.grid_size") == 0)
-				{
-					_grid_size = Util::string_to_double(definition[1]);
-				}
-				else if (definition[0].compare("moovie.tick_label_font_size") == 0)
-				{
-					_tick_label = TextProperties(_tick_label.fontname(), Util::string_to_double(definition[1]));
-				}
-				else if (definition[0].compare("moovie.var_label_font_size") == 0)
-				{
-					_var_label = TextProperties(_var_label.fontname(), Util::string_to_double(definition[1]));
-				}
-				else if (definition[0].compare("moovie.thick_line") == 0)
-				{
-					_prop_thick.line_width = Util::string_to_double(definition[1]);
-				}
-				else if (definition[0].compare("moovie.thick_line") == 0)
-				{
-					_prop_thin.line_width = Util::string_to_double(definition[1]);
-				}
+				_width = Util::string_to_int(value);
 			}
-			else if (std::regex_match(stripped_line, mooviec_name))
+			else if (key.compare("moovie.height") == 0)
 			{
-				const std::vector<std::string> & definition = Util::split(stripped_line, "=");
-				if (definition[0].compare("moovie.tick_label_font") == 0)
-				{
-					_tick_label = TextProperties(definition[1], _tick_label.fontsize());
-				}
-				else if (definition[0].compare("moovie.var_label_font") == 0)
-				{
-					_var_label = TextProperties(definition[1], _var_label.fontsize());
-				}
+				_height = Util::string_to_int(value);
+			}
+			else if (key.compare("moovie.output_angle_span") == 0)
+			{
+				_output_angle_span = Util::string_to_double(value);
+			}
+			else if (key.compare("moovie.output_inner_radius") == 0)
+			{
+					_output_inner_radius = Util::string_to_double(value);
+			}
+			else if (key.compare("moovie.output_thickness") == 0)
+			{
+				_output_thickness = Util::string_to_double(value);
+			}
+			else if (key.compare("moovie.grid_size") == 0)
+			{
+				_grid_size = Util::string_to_double(value);
+			}
+			else if (key.compare("moovie.num_major_ticks_coordgrid") == 0)
+			{
+				_num_major_ticks_cg = Util::string_to_int(value);
+			}
+			else if (key.compare("moovie.num_minor_ticks_coordgrid") == 0)
+			{
+					_num_minor_ticks_cg = Util::string_to_int(value);
+			}
+			else if (key.compare("moovie.num_segments") == 0)
+			{
+				_num_segments = Util::string_to_int(value);
+			}
+			else if (key.compare("moovie.input_inner_radius") == 0)
+			{
+				_input_inner_radius = Util::string_to_double(value);
+			}
+			else if (key.compare("moovie.input_thickness") == 0)
+			{
+				_input_thickness = Util::string_to_double(value);
+			}
+			else if (key.compare("moovie.input_separation_angle") == 0)
+			{
+				_input_separation_angle = Util::string_to_double(value);
+			}
+			else if (key.compare("moovie.num_major_ticks_varaxis") == 0)
+			{
+				_num_major_ticks_va = Util::string_to_int(value);
+			}
+			else if (key.compare("moovie.num_minor_ticks_varaxis") == 0)
+			{
+				_num_major_ticks_va = Util::string_to_int(value);
+			}
+			else if (key.compare("moovie.num_histogram_classes") == 0)
+			{
+				_num_histogram_classes = Util::string_to_int(value);
+			}
+			else if (key.compare("moovie.histogram_height") == 0)
+			{
+				_histogram_height = Util::string_to_double(value);
+			}
+			else if (key.compare("moovie.histogram_background") == 0)
+			{
+				std::vector<std::string> values = Util::split(value, ",");
+				if (values.size() < 3);
+				_histogram_background = Color(
+						Util::string_to_double(values[0]),
+						Util::string_to_double(values[1]),
+						Util::string_to_double(values[2])
+				);
+			}
+			else if (key.compare("moovie.line_width_datalink") == 0)
+			{
+				_line_with_dl = Util::string_to_double(value);
+			}
+			else if (key.compare("moovie.ratio_connector_arc") == 0)
+			{
+				_ratio_connector_arc = Util::string_to_double(value);
+			}
+			else if (key.compare("moovie.tick_label_font_size") == 0)
+			{
+				_tick_label = TextProperties(_tick_label.fontname(), Util::string_to_double(value));
+			}
+			else if (key.compare("moovie.var_label_font_size") == 0)
+			{
+				_var_label = TextProperties(_var_label.fontname(), Util::string_to_double(value));
+			}
+			else if (key.compare("moovie.thick_line") == 0)
+			{
+				_prop_thick.line_width = Util::string_to_double(value);
+			}
+			else if (key.compare("moovie.thick_line") == 0)
+			{
+				_prop_thin.line_width = Util::string_to_double(value);
+			}
+			else if (key.compare("moovie.tick_label_font") == 0)
+			{
+				_tick_label = TextProperties(value, _tick_label.fontsize());
+			}
+			else if (key.compare("moovie.var_label_font") == 0)
+			{
+				_var_label = TextProperties(value, _var_label.fontsize());
 			}
 		}
 	}
--- Configuration.h
+++ Configuration.h
@@ -23,6 +23,13 @@ public:
 	static const double RADIAL_TEXT_FACTOR;
 
 	static const double COORDGRID_ADJUSTMENT;
+	static const double COORDPOINT_ANGLE;
+
+	static const double END_RADIUS_MAJOR_FACTOR;
+	static const double END_RADIUS_MINOR_FACTOR;
+	static const double RADIUS_TICK_LABEL_FACTOR;
+	static const double RADIUS_LABEL_FACTOR;
+	static const double RADIUS_HISTOGRAM_FACTOR;
 
 	static const double DATA_LINK_LINE_WIDTH;
 
@@ -30,6 +37,8 @@ public:
 	static const double TEXT_DELTA;
 	static const double ANGLE_DELTA_SMALL;
 	static const double ANGLE_DELTA_LARGE;
+	static const double RADIUS_DELTA_SMALL;
+	static const double RADIUS_DELTA_LARGE;
 private:
 	static std::shared_ptr<Configuration> _instance;
 public:
@@ -39,6 +48,10 @@ public:
 	}
 
 	static void initialize(const std::string & fname, std::size_t inputs, const std::string & cpath = ".");
+
+
+	// Generell
+
 
 	inline const std::string & get_input_file() const
 	{
@@ -68,35 +81,9 @@ public:
 		return _height;
 	}
 
-	inline double get_input_inner_radius() const
-	{
-		return _input_inner_radius;
-	}
 
-	inline void set_input_inner_radius(double input_inner_radius)
-	{
-		_input_inner_radius = input_inner_radius;
-	}
+	// CoordGrid
 
-	inline double get_input_separation_angle() const
-	{
-		return _input_separation_angle;
-	}
-
-	inline void set_input_separation_angle(double input_separation_angle)
-	{
-		_input_separation_angle = input_separation_angle;
-	}
-
-	inline double get_input_thickness() const
-	{
-		return _input_thickness;
-	}
-
-	inline void set_input_thickness(double input_thickness)
-	{
-		_input_thickness = input_thickness;
-	}
 
 	double get_output_angle_span () const
 	{
@@ -108,68 +95,210 @@ public:
 	    _output_angle_span = output_angle_span;
 	}
 
-	inline double get_output_inner_radius() const
+	double get_output_inner_radius() const
 	{
 		return _output_inner_radius;
 	}
 
-	inline void set_output_inner_radius(double output_inner_radius)
+	void set_output_inner_radius(double output_inner_radius)
 	{
 		_output_inner_radius = output_inner_radius;
 	}
 
-	inline double get_output_thickness() const {
+	double get_output_thickness() const {
 		return _output_thickness;
 	}
 
-	inline void set_output_thickness(double output_thickness)
+	void set_output_thickness(double output_thickness)
 	{
 		_output_thickness = output_thickness;
 	}
 
-	inline double get_grid_size() const
+	double get_grid_size() const
 	{
 		return _grid_size;
 	}
 
-	inline void set_grid_size(double grid_size)
+	void set_grid_size(double grid_size)
 	{
 		_grid_size = grid_size;
 	}
 
-	inline const DrawerProperties<> & get_prop_thick() const
+	int get_num_major_ticks_cg() const
+	{
+		return _num_major_ticks_cg;
+	}
+
+	void set_num_major_ticks_cg(int num_major_ticks_cg)
+	{
+		_num_major_ticks_cg = num_major_ticks_cg;
+	}
+
+	int get_num_minor_ticks_cg() const
+	{
+		return _num_minor_ticks_cg;
+	}
+
+	void set_num_minor_ticks_cg(int num_minor_ticks_cg)
+	{
+		_num_minor_ticks_cg = num_minor_ticks_cg;
+	}
+
+	int get_num_segments() const
+	{
+		return _num_segments;
+	}
+
+	void set_num_segments(int num_segments)
+	{
+		_num_segments = num_segments;
+	}
+
+
+	// VarAxis
+
+
+	double get_input_inner_radius() const
+	{
+		return _input_inner_radius;
+	}
+
+	void set_input_inner_radius(double input_inner_radius)
+	{
+		_input_inner_radius = input_inner_radius;
+	}
+
+	double get_input_separation_angle() const
+	{
+		return _input_separation_angle;
+	}
+
+	void set_input_separation_angle(double input_separation_angle)
+	{
+		_input_separation_angle = input_separation_angle;
+	}
+
+	double get_input_thickness() const
+	{
+		return _input_thickness;
+	}
+
+	void set_input_thickness(double input_thickness)
+	{
+		_input_thickness = input_thickness;
+	}
+
+	int get_num_major_ticks_va() const
+	{
+		return _num_major_ticks_va;
+	}
+
+	void set_num_major_ticks_va(int num_major_ticks_va)
+	{
+		_num_major_ticks_va = num_major_ticks_va;
+	}
+
+	int get_num_minor_ticks_va() const
+	{
+		return _num_minor_ticks_va;
+	}
+
+	void set_num_minor_ticks_va(int num_minor_ticks_va)
+	{
+		_num_minor_ticks_va = num_minor_ticks_va;
+	}
+
+	int get_num_histogram_classes() const
+	{
+		return _num_histogram_classes;
+	}
+
+	void set_num_histogram_classes(int num_histogram_classes)
+	{
+		_num_histogram_classes = num_histogram_classes;
+	}
+
+	double get_histogram_height() const
+	{
+		return _histogram_height;
+	}
+
+	void set_histogram_height(double histogram_height)
+	{
+		_histogram_height = histogram_height;
+	}
+
+	const Color& get_histogram_background() const
+	{
+		return _histogram_background;
+	}
+
+	void set_histogram_background(const Color& histogram_background)
+	{
+		_histogram_background = histogram_background;
+	}
+
+
+	// DataLink
+
+
+	double get_line_with_dl() const
+	{
+		return _line_with_dl;
+	}
+
+	void set_line_with_dl(double line_with_dl = 0.1)
+	{
+		_line_with_dl = line_with_dl;
+	}
+
+	double get_ratio_connector_arc() const
+	{
+		return _ratio_connector_arc;
+	}
+
+	void set_ratio_connector_arc(double ratio_connector_arc)
+	{
+		_ratio_connector_arc = ratio_connector_arc;
+	}
+
+
+	// Properties and labels
+
+
+	const DrawerProperties<> & get_prop_thick() const
 	{
 		return _prop_thick;
 	}
 
-	inline void set_propthick(const DrawerProperties<> & prop_thick)
+	void set_propthick(const DrawerProperties<> & prop_thick)
 	{
 		_prop_thick = prop_thick;
 	}
 
-	inline const DrawerProperties<> & get_prop_thin() const {
+	const DrawerProperties<> & get_prop_thin() const {
 		return _prop_thin;
 	}
 
-	inline void set_prop_thin(const DrawerProperties<> & prop_thin) {
+	void set_prop_thin(const DrawerProperties<> & prop_thin) {
 		_prop_thin = prop_thin;
 	}
 
-	inline const TextProperties & get_tick_label() const {
+	const TextProperties & get_tick_label() const {
 		return _tick_label;
 	}
 
-	inline void set_ticklabel(const TextProperties & tick_label)
+	void set_ticklabel(const TextProperties & tick_label)
 	{
 		_tick_label = tick_label;
 	}
 
-	inline const TextProperties & get_var_label() const
+	const TextProperties & get_var_label() const
 	{
 		return _var_label;
 	}
 
-	inline void set_var_label(const TextProperties& var_label)
+	void set_var_label(const TextProperties& var_label)
 	{
 		_var_label = var_label;
 	}
@@ -180,40 +309,47 @@ private:
 
 	/* Absolute file paths for data (input) and image (output) */
 	std::string 		_input_file;
-	std::string		_output_file 		= "image.svg";
+	std::string			_output_file 			= "image.svg";
 
 	/* Number of input variables in the data set */
-	std::size_t		_num_inputs;
+	std::size_t			_num_inputs;
 
 	/* Width and height of the output image */
-	int 			_width 			= 500;
-	int 			_height 		= 500;
-
-	/* VarAxis modification values */
-	double 			_input_inner_radius 	= 150;
-	double 			_input_thickness 	= 10;
-	double 			_input_separation_angle = 5;
+	int 				_width 					= 500;
+	int 				_height 				= 500;
 
 	/* CoordGrid modification values */
-	double			_output_angle_span	= 100;
-	double 			_output_inner_radius 	= 160;
-	double 			_output_thickness 	= 10;
-	double 			_grid_size 		= 150;
+	double				_output_angle_span		= 100;
+	double 				_output_inner_radius 	= 160;
+	double 				_output_thickness 		= 10;
+	double 				_grid_size 				= 150;
+	int					_num_major_ticks_cg		= 10;
+	int					_num_minor_ticks_cg		= 10;
+	int					_num_segments			= 10;
+
+	/* VarAxis modification values */
+	double 				_input_inner_radius 	= 150;
+	double 				_input_thickness 		= 10;
+	double 				_input_separation_angle = 5;
+	int					_num_major_ticks_va		= 10;
+	int					_num_minor_ticks_va		= 10;
+
+	/* Histogram modification values */
+	int					_num_histogram_classes	= 10;
+	double				_histogram_height		= 20;
+	Color				_histogram_background;
+
+	/* DataLink modification values */
+	double				_line_with_dl			= 0.1;
+	double				_ratio_connector_arc	= 0.6;
 
 	/* General line and fill styles */
-	DrawerProperties<> 	_prop_thick 		= {0.5, Color::BLACK, Color::BLACK};
-	DrawerProperties<> 	_prop_thin 		= {0.1, Color::BLACK, Color::BLACK};
+	DrawerProperties<> 	_prop_thick 			= {0.5, Color::BLACK, Color::BLACK};
+	DrawerProperties<> 	_prop_thin 				= {0.1, Color::BLACK, Color::BLACK};
 
 	/* General font and font size */
-	TextProperties 		_tick_label 		= {"Liberation Serif", 8};
-	TextProperties 		_var_label 		= {"Liberation Serif", 20};
-
-	/* Regex for recognizing integer, floating point
-	 * and name constants
-	 */
-	const std::regex 	mooviec_int;
-	const std::regex 	mooviec_dbl;
-	const std::regex 	mooviec_name;
+	TextProperties 		_tick_label 			= {"Liberation Serif", 8};
+	TextProperties 		_var_label 				= {"Liberation Serif", 20};
 };
 
 #endif /* CONFIGURATION_H_ */
