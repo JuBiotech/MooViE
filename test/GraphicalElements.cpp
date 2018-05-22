1ce6733ca543b54fd24791ab0f9ebf7198948eec
Anton Stratmann <a.stratmann@fz-juelich.de>
2018-05-22 16:45:47 +0200

    Added test file for graphical elements

---
+++ test/GraphicalElements.cpp
@@ -0,0 +1,24 @@
+#include "../DomainAxis.h"
+#include "../CodomainGrid.h"
+#include "../RelationElement.h"
+#include "../CairoDrawer.h"
+#include "../Scene.h"
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE "Graphical elements test"
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(domainaxis_test)
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(codomaingrid_test)
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(relationelement_test)
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(cairodrawer_test)
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(scene_test)
+BOOST_AUTO_TEST_SUITE_END()
