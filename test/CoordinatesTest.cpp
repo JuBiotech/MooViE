f3f00fb42a88fdf40fc298437f4e2f941370dba2
Anton Stratmann <a.stratmann@fz-juelich.de>
2018-05-29 15:02:13 +0200

    Refactor CoordinatesTest
    With these changes
    - there is only test suite per construct (class, struct, namespace)
    - there is only one test case per aspect (construction, arithmetics, assignment, etc.)

--- test/CoordinatesTest.cpp
+++ test/CoordinatesTest.cpp
@@ -4,55 +4,50 @@
 #define BOOST_TEST_MODULE "Coordinates test"
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_SUITE(angle_helper_conversion)
+BOOST_AUTO_TEST_SUITE(angle_helper_test)
 
-BOOST_AUTO_TEST_CASE(deg_to_rad_0)
+BOOST_AUTO_TEST_CASE(rad_to_deg)
 {
 	BOOST_CHECK_EQUAL(angle_helper::deg_to_rad(0), 0);
-}
 
-BOOST_AUTO_TEST_CASE(deg_to_rad_180)
-{
 	BOOST_CHECK_EQUAL(angle_helper::deg_to_rad(180), M_PI);
 }
 
-BOOST_AUTO_TEST_CASE(rad_to_deg_0)
+BOOST_AUTO_TEST_CASE(deg_to_rad)
 {
 	BOOST_CHECK_EQUAL(angle_helper::rad_to_deg(0), 0);
-}
 
-BOOST_AUTO_TEST_CASE(rad_to_deg_pi)
-{
 	BOOST_CHECK_EQUAL(angle_helper::rad_to_deg(M_PI), 180);
 }
 
-BOOST_AUTO_TEST_CASE(rad_to_deg_to_rad)
+BOOST_AUTO_TEST_CASE(reflexivity)
 {
 	double radians = 0.5;
 	BOOST_CHECK_EQUAL(
 			angle_helper::rad_to_deg(angle_helper::deg_to_rad(radians)),
-			radians);
-}
+			radians
+	);
 
-BOOST_AUTO_TEST_CASE(deg_to_rad_to_deg)
-{
 	double degree = 75;
 	BOOST_CHECK_EQUAL(
-			angle_helper::deg_to_rad(angle_helper::rad_to_deg(degree)), degree);
+			angle_helper::deg_to_rad(angle_helper::rad_to_deg(degree)),
+			degree
+	);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
+
 
 BOOST_AUTO_TEST_SUITE(angle_helper_distance)
 
 BOOST_AUTO_TEST_CASE(dist_to_0)
 {
-	BOOST_CHECK_EQUAL(angle_helper::rad_dist(0,M_PI), M_PI);
+	BOOST_CHECK_EQUAL(angle_helper::rad_dist(0, M_PI), M_PI);
 }
 
 BOOST_AUTO_TEST_CASE(dist_to_negative)
 {
-	BOOST_CHECK_EQUAL(angle_helper::rad_dist(M_PI,0), M_PI);
+	BOOST_CHECK_EQUAL(angle_helper::rad_dist(M_PI, 0), M_PI);
 }
 
 BOOST_AUTO_TEST_CASE(offset)
@@ -66,55 +61,40 @@ BOOST_AUTO_TEST_CASE(offset)
 
 BOOST_AUTO_TEST_SUITE_END()
 
-BOOST_AUTO_TEST_SUITE(Angle_base_test)
+BOOST_AUTO_TEST_SUITE(angle_test)
 
 BOOST_AUTO_TEST_CASE(constructor)
 {
 	double angle = 0.5;
 	BOOST_CHECK_EQUAL(Angle(angle).value(), angle);
-}
 
-BOOST_AUTO_TEST_CASE(constructor_wrap_large)
-{
 	double angle_wrapped = 0.5;
-	double angle = angle_wrapped + 2.0 * M_PI;
+	angle = angle_wrapped + 2.0 * M_PI;
 	BOOST_CHECK_EQUAL(Angle(angle).value(), angle_wrapped);
-}
 
-BOOST_AUTO_TEST_CASE(constructor_wrap_negative)
-{
-	double angle_wrapped = 0.5;
-	double angle = angle_wrapped - 2.0 * M_PI;
+	angle_wrapped = 0.5;
+	angle = angle_wrapped - 2.0 * M_PI;
 	BOOST_CHECK_EQUAL(Angle(angle).value(), angle_wrapped);
-}
 
-BOOST_AUTO_TEST_CASE(equality)
-{
-	double angle = 43;
-	Angle a(angle);
-	Angle b(angle);
+	angle = 43;
+	Angle a = angle;
+	Angle b = angle;
 	BOOST_CHECK_EQUAL(a, b);
-}
 
-BOOST_AUTO_TEST_CASE(equality_wrap)
-{
-	double angle = 21;
-	Angle a(angle);
-	Angle b(angle + 2 * M_PI);
+	angle = 21;
+	a = angle;
+	b = angle + 2 * M_PI;
 	BOOST_CHECK_EQUAL(a, b);
-}
 
-BOOST_AUTO_TEST_CASE(equality_programatic)
-{
-	double angle = 0;
+	angle = 0;
 	double step = 0.0123;
 	double wrap_arround = 0;
 	while (angle < 2 * M_PI)
 	{
 		while (wrap_arround < 10)
 		{
-			Angle a(angle);
-			Angle b(angle + wrap_arround * 2.0 * M_PI);
+			a = angle;
+			b = angle + wrap_arround * 2.0 * M_PI;
 			BOOST_CHECK_EQUAL(a, b);
 			wrap_arround += 1;
 		}
@@ -122,7 +102,7 @@ BOOST_AUTO_TEST_CASE(equality_programatic)
 	}
 }
 
-BOOST_AUTO_TEST_CASE(assign_operator)
+BOOST_AUTO_TEST_CASE(assignment_operator)
 {
 	double angle = 127.3;
 	Angle a(angle);
@@ -131,108 +111,76 @@ BOOST_AUTO_TEST_CASE(assign_operator)
 	BOOST_CHECK_EQUAL(a, b);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
-
-BOOST_AUTO_TEST_SUITE(Angle_inequality_test)
-
-BOOST_AUTO_TEST_SUITE_END()
-
-BOOST_AUTO_TEST_SUITE(Angle_arithmetic_test)
-
-BOOST_AUTO_TEST_CASE(addition)
+BOOST_AUTO_TEST_CASE(arithmetic_operators)
 {
 	double angle = 0.5;
 	double add = 0.2;
 	Angle a(angle);
 	Angle b(add);
 	BOOST_CHECK_EQUAL((a + b).value(), angle + add);
-}
 
-BOOST_AUTO_TEST_CASE(addition_wrap_to_0)
-{
-	double angle = M_PI;
-	double add = M_PI;
-	Angle a(angle);
-	Angle b(add);
+	angle = M_PI;
+	add = M_PI;
+	a = angle;
+	b = add;
 	BOOST_CHECK_EQUAL((a + b).value(), 0);
-}
 
-BOOST_AUTO_TEST_CASE(addition_wrap)
-{
-	double angle = 17;
-	double add = 33;
-	Angle a(angle);
-	Angle b(add);
+	angle = 17;
+	add = 33;
+	a = angle;
+	b = add;
 	BOOST_CHECK_EQUAL(a + b, Angle(angle + add));
-}
 
-BOOST_AUTO_TEST_CASE(addition_programatic)
-{
-	double angle = 0;
-	double add = 0;
+	angle = 0;
+	add = 0;
 	double step = 0.0123;
 	while (angle < 2 * M_PI)
 	{
 		add = 0;
 		while (add < 2 * M_PI)
 		{
-			Angle a(angle);
-			Angle b(add);
+			a = angle;
+			b = add;
 			BOOST_CHECK_EQUAL(a + b, Angle(angle + add));
 			add += step;
 		}
 		angle += step;
 	}
-}
 
-BOOST_AUTO_TEST_CASE(substraction)
-{
-	double angle = 0.7;
+	angle = 0.7;
 	double minus = 0.2;
-	Angle a(angle);
-	Angle b(minus);
+	a = angle;
+	b = minus;
 	BOOST_CHECK_EQUAL((a - b).value(), angle - minus);
-}
 
-BOOST_AUTO_TEST_CASE(substraction_wrap_to_0)
-{
-	double angle = M_PI;
-	double minus = 3.0 * M_PI;
-	Angle a(angle);
-	Angle b(minus);
+	angle = M_PI;
+	minus = 3.0 * M_PI;
+	a = angle;
+	b = minus;
 	BOOST_CHECK_EQUAL((a - b).value(), 0);
-}
 
-BOOST_AUTO_TEST_CASE(substraction_wrap)
-{
-	double angle = 17;
-	double minus = 33;
-	Angle a(angle);
-	Angle b(minus);
+	angle = 17;
+	minus = 33;
+	a = angle;
+	b = minus;
 	BOOST_CHECK_EQUAL(a - b, Angle(angle - minus));
-}
 
-BOOST_AUTO_TEST_CASE(substraction_programatic)
-{
-	double angle = 0;
-	double minus = 0;
-	double step = 0.0123;
+	angle = 0;
+	minus = 0;
+	step = 0.0123;
 	while (angle < 2.0 * M_PI)
 	{
 		minus = 0;
 		while (minus < 2.0 * M_PI)
 		{
-			Angle a(angle);
-			Angle b(minus);
+			a = angle;
+			b = minus;
 			BOOST_CHECK_EQUAL(a - b, Angle(angle - minus));
 			minus += step;
 		}
 		angle += step;
 	}
-}
 
-BOOST_AUTO_TEST_CASE(add_subs_programmatic)
-{
 	double angle1 = 0;
 	double angle2 = 0;
 	double step1 = 0.0123;
@@ -242,92 +190,70 @@ BOOST_AUTO_TEST_CASE(add_subs_programmatic)
 	{
 		while (angle2 < 4.0 * M_PI)
 		{
-			Angle a(angle1);
-			Angle b(angle2);
+			a = angle1;
+			b = angle2;
 			BOOST_CHECK_EQUAL((a + b) - b, a);
 			angle2 += step2;
 		}
 		angle1 += step1;
 	}
-}
 
-BOOST_AUTO_TEST_CASE(subs_add_programmatic)
-{
-	double angle1 = 0;
-	double angle2 = 0;
-	double step1 = 0.0123;
-	double step2 = 0.0456 / 2.0;
+	angle1 = 0;
+	angle2 = 0;
+	step1 = 0.0123;
+	step2 = 0.0456 / 2.0;
 
 	while (angle1 < 4.0 * M_PI)
 	{
 		while (angle2 < 4.0 * M_PI)
 		{
-			Angle a(angle1);
-			Angle b(angle2);
+			a = angle1;
+			b = angle2;
 			BOOST_CHECK_EQUAL((a - b) + b, a);
 			angle2 += step2;
 		}
 		angle1 += step1;
 	}
-}
 
-BOOST_AUTO_TEST_CASE(multiplication)
-{
-	double angle = 0.5;
+	angle = 0.5;
 	double times = 2;
-	Angle a(angle);
+	a = angle;
 	BOOST_CHECK_EQUAL((a * times).value(), angle * times);
-}
 
-BOOST_AUTO_TEST_CASE(multiplication_wrap_to_0)
-{
-	double angle = M_PI;
-	double times = 2;
-	Angle a(angle);
+	angle = M_PI;
+	times = 2;
+	a = angle;
 	BOOST_CHECK_EQUAL((a * times).value(), 0);
-}
 
-BOOST_AUTO_TEST_CASE(multiplication_wrap)
-{
-	double angle = 17;
-	double times = 33;
-	Angle a(angle);
+	angle = 17;
+	times = 33;
+	a = angle;
 	BOOST_CHECK_CLOSE((a * times).value(), Angle(angle * times).value(), 0.000001);
-}
 
-BOOST_AUTO_TEST_CASE(multiplication_programatic)
-{
-	double angle = 0;
-	double times = -100;
-	double step = 0.0123;
+	angle = 0;
+	times = -100;
+	step = 0.0123;
 	double step_times = 0.645;
 	while (angle < 2 * M_PI)
 	{
 		times = -100;
 		while (times < 100)
 		{
-			Angle a(angle);
+			a = angle;
 			BOOST_CHECK_EQUAL(a * times, Angle(angle * times));
 			times += step_times;
 		}
 		angle += step;
 	}
-}
 
-BOOST_AUTO_TEST_CASE(division)
-{
-	double angle = 0.8;
+	angle = 0.8;
 	double by = 2.0;
-	Angle a(angle);
+	a = angle;
 	BOOST_CHECK_EQUAL((a / by).value(), angle / by);
-}
 
-BOOST_AUTO_TEST_CASE(division_programatic)
-{
-	double angle = 0;
-	double by = -100;
-	double step = 0.0123;
-	double step_by = 0.645;
+	angle = 0;
+	by = -100;
+	step = 0.0123;
 	while (angle < 2.0 * M_PI)
 	{
 		by = -100;
@@ -335,45 +261,39 @@ BOOST_AUTO_TEST_CASE(division_programatic)
 		{
 			if (by == 0)
 				by += 0.1;
-			Angle a(angle);
+			a = angle;
 			BOOST_CHECK_EQUAL(a / by, Angle(angle / by));
 			by += step;
 		}
 		angle += step;
 	}
-}
 
-BOOST_AUTO_TEST_CASE(times_by_programmatic)
-{
-	double angle = 0;
-	double times = -100;
+	angle = 0;
+	times = -100;
 	double step_angle = 0.0123;
-	double step_times = 0.0456 / 2.0;
+	step_times = 0.0456 / 2.0;
 
 	while (angle < 4.0 * M_PI)
 	{
 		while (times < 100)
 		{
-			Angle a(angle);
+			a = angle;
 			BOOST_CHECK_EQUAL((a * times) / times, a);
 			times += step_times;
 		}
 		angle += step_angle;
 	}
-}
 
-BOOST_AUTO_TEST_CASE(by_times_programmatic)
-{
-	double angle = 0;
-	double by = -100;
-	double step_angle = 0.0123;
-	double step_times = 0.0456 / 2.0;
+	angle = 0;
+	by = -100;
+	step_angle = 0.0123;
+	step_times = 0.0456 / 2.0;
 
 	while (angle < 4.0 * M_PI)
 	{
 		while (by < 100)
 		{
-			Angle a(angle);
+			a = angle;
 			BOOST_CHECK_EQUAL((a / by) * by, a);
 			by += step_times;
 		}
@@ -389,12 +309,9 @@ BOOST_AUTO_TEST_CASE(center)
 	Angle b(angle2);
 	Angle c = Angle::center(a, b);
 	BOOST_CHECK_EQUAL(c - a, b - c);
-}
 
-BOOST_AUTO_TEST_CASE(center_programatic)
-{
-	double angle1 = -4.0 * M_PI;
-	double angle2 = 4.0 * M_PI;
+	angle1 = -4.0 * M_PI;
+	angle2 = 4.0 * M_PI;
 	double step1 = 0.0123;
 	double step2 = 0.0234;
 	while (angle1 < 4.0 * M_PI)
@@ -402,8 +319,8 @@ BOOST_AUTO_TEST_CASE(center_programatic)
 		angle2 = 4.0 * M_PI;
 		while (angle2 > -4.0 * M_PI)
 		{
-			Angle a(angle1);
-			Angle b(angle2);
+			a = angle1;
+			b = angle2;
 			Angle c = Angle::center(a, b);
 			BOOST_CHECK_CLOSE((c - a).value(), (b - c).value(), 0.00001);
 			angle2 -= step2;
@@ -412,7 +329,7 @@ BOOST_AUTO_TEST_CASE(center_programatic)
 	}
 }
 
-BOOST_AUTO_TEST_CASE(interpolate_asc_extremes)
+BOOST_AUTO_TEST_CASE(interpolate)
 {
 	double angle1 = 0.5;
 	double angle2 = 1;
@@ -420,24 +337,18 @@ BOOST_AUTO_TEST_CASE(interpolate_asc_extremes)
 	Angle b(angle2);
 	BOOST_CHECK_EQUAL(Angle::interpolate(a, b, 0), a);
 	BOOST_CHECK_EQUAL(Angle::interpolate(a, b, 1), b);
-}
 
-BOOST_AUTO_TEST_CASE(interpolate_desc_extremes)
-{
-	double angle1 = 1;
-	double angle2 = 0.5;
-	Angle a(angle1);
-	Angle b(angle2);
+	angle1 = 1;
+	angle2 = 0.5;
+	a = angle1;
+	b = angle2;
 	BOOST_CHECK_EQUAL(Angle::interpolate(a, b, 0), a);
 	BOOST_CHECK_EQUAL(Angle::interpolate(a, b, 1), b);
-}
 
-BOOST_AUTO_TEST_CASE(interpolate_asc_programatic)
-{
-	double angle1 = 0.5;
-	double angle2 = 1;
-	Angle a(angle1);
-	Angle b(angle2);
+	angle1 = 0.5;
+	angle2 = 1;
+	a = angle1;
+	b = angle2;
 	double p = 0;
 	double step = 0.1;
 	p += step;
@@ -448,16 +359,13 @@ BOOST_AUTO_TEST_CASE(interpolate_asc_programatic)
 		BOOST_CHECK_CLOSE((b - c).value(), (b - a).value() * (1 - p), 0.00001);
 		p += step;
 	}
-}
 
-BOOST_AUTO_TEST_CASE(interpolate_desc_programatic)
-{
-	double angle1 = 1;
-	double angle2 = 0.5;
-	Angle a(angle1);
-	Angle b(angle2);
-	double p = 0;
-	double step = 0.1;
+	angle1 = 1;
+	angle2 = 0.5;
+	a = angle1;
+	b = angle2;
+	p = 0;
+	step = 0.1;
 	p += step;
 	while (p < 0.95)
 	{
