2770094b59672af41a5eed743b002b81156a5826
Anton Stratmann <a.stratmann@fz-juelich.de>
2018-07-16 11:59:37 +0200

    Implement function to manipulate MooViE data
    For interactivity it shall be possible to toggle, swap and restrict the
    columns of the initial data set. To handle it using the Scene class
    functions for applying this modifications have been implemented.

--- include/Scene.h
+++ include/Scene.h
@@ -14,41 +14,92 @@
 #include <DataSet.h>
 #include <Configuration.h>
 
-/** Scene constructs a diagram that displays data vectors
+/** Scene class represents a MooViE scene. It is initially drawn and every alteration
+ * will require an update to be persistent.
+ *
  * @brief The Scene class
+ *
  * @author beyss
  * @date 28.08.2017
  */
 class Scene
 {
 public:
-    /** Creates a new MooViE Scene.
-     * @brief Scene
+    /** Creates a new MooViE Scene and draws it immediately. It is required to first
+     * initialize a Configuration.
      */
     Scene();
 
+    /** Reinitializes all components and redraws the MooViE scene.
+     */
     void update(void);
 
+    /** Enables/disables the input with the given index. The scene needs to be updated
+     * afterwards.
+     *
+     * @param index the input index
+     * @param mode set enabled or disabled
+     *
+     * @throw out_of_bounds if index is incorrect
+     */
     void toggle_input(std::size_t index, bool mode);
 
+    /** Enables/disables the output with the given index. The scene needs to be
+     * updated afterwards.
+     *
+     * @param index the output index
+     * @param mode set enabled or disabled
+     *
+     * @throw out_of_bounds if index is incorrect
+     */
     void toggle_output(std::size_t index, bool mode);
 
-    void restrict_input(std::size_t index, double upper_restr, double lower_restr);
+    /** Rearranges the order of inputs in this MooViE scene by swapping the inputs
+     * with the given indices. The scene needs to be updated afterwards.
+     *
+     * @param from_index the first inputs index
+     * @param to_index the second inputs index
+     *
+     * @throw out_of_bounds if indices are incorrect
+     */
+    void swap_inputs(std::size_t from_index, std::size_t to_index);
 
-    void restrict_output(std::size_t index, double upper_restr, double lower_restr);
+    /** Rearranges the order of outputs in this MooViE scene by swapping the outputs
+     * with the given indices. The scene needs to be updated afterwards.
+     *
+     * @param from_index the first outputs index
+     * @param to_index the second outputs index
+     *
+     * @throw out_of_bounds if indices are incorrect
+     */
+    void swap_outputs(std::size_t from_index, std::size_t to_index);
 
-    void change_input_order(std::size_t from_index, std::size_t to_index);
+    /** Restricts the input with given index to a given interval. Every row
+     * whose associated input value is not in the interval will be disabled.
+     * The scene needs to be updated afterwards.
+     *
+     * @param index the input index
+     * @param lower_restr the lower bound
+     * @param upper_restr the upper bound
+     */
+    void restrict_input(std::size_t index, double lower_restr, double upper_restr);
 
-    void change_output_order(std::size_t from_index, std::size_t to_index);
+    /** Restricts the output with given index to a given interval. Every row
+     * whose associated output value is not in the interval will be disabled.
+     * The scene needs to be updated afterwards.
+     *
+     * @param index the output index
+     * @param lower_restr the lower bound
+     * @param upper_restr the upper bound
+     */
+    void restrict_output(std::size_t index, double lower_restr, double upper_restr);
 
 private:
-    /** Draws the Scene components CoordGrid, VarAxis' and DataLinks.
-     * @brief draw_components
+    /** Draws the Scene components CodomainGrid, VariableAxis' and RelationElements.
      */
     void draw_components(void);
 
-    /**
-     *
+    /** Initializes DomainAxis' and RelationElements.
      */
     void initialize(void);
 private:
@@ -60,9 +111,9 @@ private:
 
     /** Output data section of the diagram */
     CodomainGrid 								grid;
-    /** Variable axis */
+    /** Input data sections of the diagram */
     std::vector<DomainAxis> 					axis;
-    /** Relation elements*/
+    /** Relation elements */
     std::vector<RelationElement> 				links;
 };
 
