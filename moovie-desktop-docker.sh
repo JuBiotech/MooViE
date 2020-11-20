48079e045abc976b8b3f378899af25eab110d588
stratmann <a.o.stratmann@gmail.com>
2020-11-20 16:01:30 +0100

    Add simple script build and run docker container

---
+++ moovie-desktop-docker.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Build docker image if not already build
+if [[ "$(docker images -q moovie-desktop 2>/dev/null)" == "" ]]
+then
+  # Set the docker file path
+  DOCKERFILE_LOC=$(pwd)
+  if [[ $# -gt 1 ]]; then
+    DOCKERFILE_LOC=$1
+  fi
+  echo "Using docker file location \"$DOCKERFILE_LOC\""
+
+  echo "Building docker image (This can take a while)"
+  docker build -t moovie-desktop $DOCKERFILE_LOC > /dev/null
+fi
+
+# Run docker image with moovie-desktop
+echo "Run docker image"
+docker run \
+  --user "$(id -u)" \
+  --workdir "$(pwd)" \
+  --volume /home/$USER/:/home/$USER \
+  --volume /etc/group:/etc/group:ro \
+  --volume /etc/passwd:/etc/passwd:ro \
+  --volume /etc/shadow:/etc/shadow:ro \
+  --volume /etc/sudoers.d:/etc/sudoers.d:ro \
+  --volume /tmp/.X11-unix:/tmp/.X11-unix \
+  --env DISPLAY=unix$DISPLAY \
+  moovie-desktop
