0e503960f3d385b5406300eb5fb0dce27d615e68
stratmann <a.o.stratmann@gmail.com>
2021-02-16 12:47:22 +0100

    Remove unnecessary code

--- moovie-desktop-docker.sh
+++ moovie-desktop-docker.sh
@@ -20,10 +20,6 @@ docker run \
   --user "$(id -u)" \
   --workdir "$(pwd)" \
   --volume /home/$USER/:/home/$USER \
-  --volume /etc/group:/etc/group:ro \
-  --volume /etc/passwd:/etc/passwd:ro \
-  --volume /etc/shadow:/etc/shadow:ro \
-  --volume /etc/sudoers.d:/etc/sudoers.d:ro \
   --volume /tmp/.X11-unix:/tmp/.X11-unix \
-  --env DISPLAY=unix$DISPLAY \
+  --env DISPLAY=$DISPLAY \
   moovie-desktop
