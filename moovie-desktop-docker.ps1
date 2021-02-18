66384a472f1e5c581e61f492374b3fe6f6c07266
stratmann <a.o.stratmann@gmail.com>
2021-02-18 17:49:22 +0100

    Add parameter for including volumes

--- moovie-desktop-docker.ps1
+++ moovie-desktop-docker.ps1
@@ -1,22 +1,29 @@
 param (
-    [string]$DOCKERFILE_LOC = $(Get-Location).ToString()
+    [Alias("d", "dockerfile-loc")][string]$DOCKERFILE_LOC = $(Get-Location).ToString(),
+    [Alias("v", "volume-loc")][string]$VOLUME_LOC = "C:\Users\$($env:UserName)"
 )
 
 # Build docker image if not already build
 docker images -q moovie-desktop 2>&1 | Out-Null
 if ( !$? )
 {
-    Write-Output "Using docker file location '$DOCKERFILE_LOC'"
+    # Resolve full dockerfile location path
+    $DOCKERFILE_LOC = $((Resolve-Path $DOCKERFILE_LOC).ToString())
+    Write-Output "Using Dockerfile location '$DOCKERFILE_LOC'"
 
     Write-Output "Building docker image (This can take a while)"
     docker build -t moovie-desktop $DOCKERFILE_LOC | Out-Null
 }
 
+# Resolve full volume path
+$VOLUME_LOC = $((Resolve-Path $VOLUME_LOC).ToString())
+Write-Output "Include volume $VOLUME_LOC"
+
 # Run docker image with moovie-desktop
 Write-Output "Run docker image"
 docker run `
     --user 1000 `
-    --workdir $("//c" + $(Get-Location).ToString().TrimStart("C:").Replace('\', '/')) `
-    --volume /home/$USER/://c/home/$USER `
-    --env DISPLAY=host.docker.internal:0 `
+    --workdir /home/workspace `
+    --volume ${VOLUME_LOC}:/home/workspace `
+    --env DISPLAY=host.docker.internal:0.0 `
     moovie-desktop 2> Out-Null
