d178b8d0b34ef10ff51a3b4f944a6603b438c192
stratmann <a.o.stratmann@gmail.com>
2021-03-02 10:43:53 +0100

    Resolve correct paths of command line args

--- moovie-desktop-docker.sh
+++ moovie-desktop-docker.sh
@@ -25,16 +25,17 @@ done
 # Build docker image if not already build
 if [[ "$(docker images -q moovie-desktop 2>/dev/null)" == "" ]]
 then
-  # Set the docker file path
-  DOCKERFILE_LOC=$(pwd)
-  if [[ $# -gt 1 ]]; then
-    DOCKERFILE_LOC=$1
-  fi
+  # Resolve full dockerfile location path
+  DOCKERFILE_LOC=$(readlink -e $DOCKERFILE_LOC)
   echo "Using docker file location \"$DOCKERFILE_LOC\""
 
   echo "Building docker image (This can take a while)"
   docker build -t moovie-desktop $DOCKERFILE_LOC > /dev/null
 fi
+
+# Resolve full volume path
+VOLUME_LOC=$(readlink -e $VOLUME_LOC)
+echo "Include volume $VOLUME_LOC"
 
 # Run docker image with moovie-desktop
 echo "Run docker image"
@@ -43,5 +44,5 @@ docker run \
   --workdir "$(pwd)" \
   --volume /home/$USER/:/home/$USER \
   --volume /tmp/.X11-unix:/tmp/.X11-unix \
-  --env DISPLAY=$DISPLAY \
+  --env DISPLAY="$DISPLAY" \
   moovie-desktop 2> /dev/null
