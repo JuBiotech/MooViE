5153fffd03b0d86bae4f4610552f5a9e2261217e
stratmann <a.o.stratmann@gmail.com>
2021-07-16 10:20:41 +0200

    Add message handler to avoid warnings
    Closes #18

--- src/main/cpp/desktop/moovie-desktop.cpp
+++ src/main/cpp/desktop/moovie-desktop.cpp
@@ -2,13 +2,31 @@
 #include <QApplication>
 #include <QIcon>
 
+void
+messageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg) {
+    std::string file(context.file ? context.file : ""),
+            function(context.function ? context.function : "");
+    switch (type) {
+        case QtDebugMsg:
+        case QtInfoMsg:
+        case QtWarningMsg:
+            break;
+        case QtCriticalMsg:
+        case QtFatalMsg:
+            std::cout << "Error: " << msg.toStdString() << " (" << file << ":" << context.line << ", " << function
+                      << ")" << std::endl;
+            break;
+    }
+}
+
 int
-main (int argc, char *argv[])
+main(int argc, char *argv[])
 {
-  QApplication moovie (argc, argv);
+    qInstallMessageHandler(messageHandler);
+  QApplication moovie(argc, argv);
 
   MainWindow window;
-  window.show ();
+  window.show();
 
-  return moovie.exec ();
+  return moovie.exec();
 }
